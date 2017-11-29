5e766d07ae9909740ea186cdcd19a91a4eb01907
stackbaxter <stackbaxter@yahoo.com>
2017-11-29 03:17:20 -0600

    back to the beginning

--- test.js
+++ test.js
@@ -64,7 +64,7 @@ let comps = [
   // let lastElem = comps.length-1;
   // console.log(lastElem);
   // let max=stuff;
-  
+  let choices = [];
   function maxAmtValue(comps, stuff) {
     let compsVal = Array(stuff + 1).fill(0);
     console.log(comps, stuff, compsVal)
@@ -91,41 +91,64 @@ let comps = [
       compsVal[curAmt] = bestPrice;
       console.log(compsVal,"compsVal" );
     }
-    console.log(choices, "unfiltered");
+
+
+    // let ar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    
+    // let amt = 15;
+    
+   
+
+   // console.log(choices, "unfiltered");
     // let amount = compsVal[stuff];
     // choice.filter(x => )
     //knew the right company had to be somwhere,
-    //think i have it, just need to filter and reduce
+    //think i have it, just need to filter and reduce...
+    //don't have it...need more falling back and regrouping.
     
-    choices.sort();
-    console.log(choices," sort")
+  //   choices.sort();
+  //   console.log(choices," sort")
     
-   let fewChoices = choices.filter(x => x);
+  //  let fewChoices = choices.filter(x => x);
       
-    fewChoices = fewChoices.filter(x => x.amt<= stuff)
-    console.log(fewChoices, " filtered");
-    let fewerChoices = fewChoices.sort(); 
-    let newAr = [];
-    for(let i = 0; i< fewerChoices.length; i ++){
+  //   fewChoices = fewChoices.filter(x => x.amt<= stuff)
+  //   console.log(fewChoices, " filtered");
+  //   let fewerChoices = fewChoices.sort(); 
+  //   let newAr = [];
+  //   for(let i = 0; i< fewerChoices.length; i ++){
     
-      if(newAr.includes(fewerChoices[i])===false){
-        newAr.push(fewerChoices[i]);
-      }
-    }
+  //     if(newAr.includes(fewerChoices[i])===false){
+  //       newAr.push(fewerChoices[i]);
+  //     }
+  //   }
       // } fewChoices.filter(x =>{
       
     // if(!fewerChoices.includes(x)){
     // return x; 
     // }
     // });
-    console.log(fewerChoices, " fewerChoices");
-    console.log(newAr," newar")
+    //console.log(fewerChoices, " fewerChoices");
+    //console.log(newAr," newar")
     return compsVal[stuff];
   }
   
   console.log(maxAmtValue(comps, 12)+"checkit");
+  let price = maxAmtValue(comps, 12);
 
+  for(let i= 0, n=comps.length-1; i< comps.length; n--, i++){
+    let j= i+1;
+    //let n= comps.length-1;
+    if(comps[i].price + comps[j].price +comps[n].price=== price){
+      console.log("yes")
+      console.log(comps[i],comps[j],comps[n]);
+    choices.push(comps[i],comps[j],comps[n]);
+      
+    }
+    
+   
+  }
 
+console.log(choices,"  choices new");
 
 // This is a great learning experience, I always start with methods similar to below, 
 // knowing in my head that there is a more better way to represent it
